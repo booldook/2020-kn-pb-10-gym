aa98005e35d0a039979fd03d0b3190f1e8a15805
booldook <booldook@gmail.com>
2020-05-27 15:53:20 +0900

    bar animation

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+/************ 이벤트콜백 *************/
 function onNaviHover() {
 	$(this).find(".subs").stop().fadeIn(500);
 }
@@ -6,4 +7,18 @@ function onNaviLeave() {
 	$(this).find(".subs").stop().fadeOut(500);
 }
 
-$(".navi-child").hover(onNaviHover, onNaviLeave);
+function onBarClick() {
+	if($(this).hasClass("default")) {
+		$(this).removeClass("default").addClass("active");
+	}
+	else if($(this).hasClass("active")) {
+		$(this).removeClass("active").addClass("default");
+	}
+	else {
+		$(this).addClass("active");
+	}
+}
+
+/************ 이벤트선언 *************/
+$(".header .navi-child").hover(onNaviHover, onNaviLeave);
+$(".header .navi-bars").click(onBarClick);
