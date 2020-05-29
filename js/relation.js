5ee2bd31f1a7c1b1f63ff27259156fa1d98f413b
booldook <booldook@gmail.com>
2020-05-29 18:14:39 +0900

    realtion.html

---
+++ js/relation.js
@@ -0,0 +1,14 @@
+function init() {
+	$(".box").each(function(i){
+		if(i > 0) {
+			setTimeout(function(){
+				tops = $(".box").eq(i).prev().offset().top + $(".box").eq(i).prev().innerHeight();
+				console.log(tops);
+				$(".box").eq(i).css("top", tops + "px");
+			}, i * 300);
+		}
+	});
+}
+
+
+$(window).resize(init).trigger("resize");
